0983556ac0da75e63ddd1b10f131c86ed4285c12
sheemasalah <sheemasalah@gmail.com>
2026-03-31 18:41:43 +0530

    fix api connection

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,18 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export const fetchTasks = () => API.get("/tasks");
+// export const createTask = (data) => API.post("/tasks", data);
+// export const toggleTask = (id) => API.patch(`/tasks/${id}/toggle`);
+// export const deleteTask = (id) => API.delete(`/tasks/${id}`);
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskflow-backend-sg4b.onrender.com/api",
 });
 
 export const fetchTasks = () => API.get("/tasks");
